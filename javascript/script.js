0aef42454e4c0bcae2ac2cbf7b6198bcdc9ae276
ShriganeshBhandari09 <shriganesh.bhandari693@gmail.com>
2024-09-29 13:26:02 +0530

    Final Commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
+const seachbar__menu = document.getElementById("seachbar__menu");
+const mobileMenu = document.getElementById("mobile__menu");
 const projectContainer = document.getElementById("project");
 const backButton = document.getElementById("back");
 const frontButton = document.getElementById("front");
 const scrollAmount = 405;
+
+seachbar__menu.onclick = function () {
+  if (mobileMenu.style.display === "flex") {
+    mobileMenu.style.display = "none";
+  } else {
+    mobileMenu.style.display = "flex";
+  }
+};
 
 function back() {
   projectContainer.scrollBy({
